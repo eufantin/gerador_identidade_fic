18f73c7da360e562a53561689b3b6bcfc973774a
Victor Fantin <vicctorfantin@gmail.com>
2026-02-03 14:16:43 -0300

    estrutura criada
    import de lib e tabelas feitas

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+nomes = [
+    "Lucas", "Marcos", "ana", "beatriz", "carlos", 
+    "fernanda", "rafael", "juliana", "pedro", "camila"
+]
+
+sobrenomes = [
+    "silva", "santos", "oliveirs", "pereira", 
+    "costa", "rodrigues", "alves", "lima"
+]
+
